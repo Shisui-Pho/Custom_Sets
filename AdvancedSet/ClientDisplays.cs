af97e3b6fa5be6e627926c189075dbc8efb18a23
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-23 10:30:27 +0200

    Added partial client classes

---
+++ AdvancedSet/ClientDisplays.cs
@@ -0,0 +1,123 @@
+﻿using SetLibrary;
+using SetLibrary.Collections;
+using SetLibrary.Generic;
+using System;
+using System.Windows.Forms;
+
+namespace AdvancedSet
+{
+    //This client will be for displaying 
+    public partial class Client
+    {
+        private static void DisplaySets(ISetCollection<int> collection)
+        {
+            Console.WriteLine();
+            foreach (Set item in collection)
+            {
+                Console.WriteLine($"\t{item.Name.PadRight(5)} : {item.ElementString.PadRight(60)}  |{item.Name}| = {item.Cardinality}");
+            }//end foreach
+            Console.WriteLine();
+            Console.WriteLine();
+        }//DisplaySets
+        private static void AddTestData(ISetCollection<int> sets)
+        {
+            //Creating the sets
+            string expression = "{{3,4,1,5,8,{6,2,5,{8,9},{9,6}}},5,6,{6,3},9,1,0}";
+            ICSet<int> set = new GenericSet<int>(expression, settings);
+            sets.Add(set);
+
+            expression = "{2,5,1,3,4,5,6}";
+            set = new GenericSet<int>(expression, settings);
+            sets.Add(set);
+
+            expression = "{{{{{{}}}}}}";
+            set = new GenericSet<int>(expression, settings);
+            sets.Add(set);
+
+            expression = "{10,{2,2,1},{6},5,2,5,5,8}";
+            set = new GenericSet<int>(expression, settings);
+            sets.Add(set);
+
+            expression = "{{2,1,7,3},{1},{2,5,5},{8,5,1,4,5},{1,2,5},{2,3}}";
+            set = new GenericSet<int>(expression, settings);
+            sets.Add(set);
+
+            expression = "{5,6,1,1,1,2,4,5,{2,1,2,3},{3,2,1,1,2,3,2,3,1,2,3}}";
+            set = new GenericSet<int>(expression, settings);
+            sets.Add(set);
+        }//AddTestData
+        private static void DisplayNewSetHeader(ISetCollection<int> collection)
+        {
+            Console.Clear();
+            Console.WriteLine("\tAdding a new set");
+            Console.WriteLine("\t=================");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\tThings to note : ");
+            Console.WriteLine("\t1. You are allowed to use integers only.");
+            Console.WriteLine("\t2. Make sure braces are matching.");
+            Console.WriteLine("\t3. Nest as much as you like, but make sure baraces are matching.");
+            Console.WriteLine("\t4. Elements must be seperated by commas.");
+            Console.WriteLine("\t    Example of a set could be : A = {1,6,8,{2,3},5,{2,6,{8}},5}");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\t***If you wish to cancel this operation clear everyting and press enter.***");
+            Console.WriteLine();
+        }//DisplayNewSetHeader
+        private static void DisplayError(string error_message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\tError : {0}", error_message);
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+        }//DisplayError
+        private static void AnyKey(string msg = "Press any key to return to the main menu....")
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("\t {0}", msg);
+            Console.ReadKey();
+        }//AnyKey
+        private static string ReadSetString(string sPrompt, string attempt = "")
+        {
+            //First clean the string to prepare it to be passed keys
+            string @new = "";
+            foreach (char character in attempt)
+            {
+                if (character == '{')
+                    @new += "{{}";
+                else if (character == '}')
+                    @new += "{}}";
+                else
+                    @new += character;
+            }//end foreach
+
+            Console.Write(sPrompt);
+            Console.CursorVisible = true;
+            try
+            {
+                //SendKeys.SendWait("This is phio");
+                SendKeys.SendWait(@new);
+            }//end try
+            catch
+            {
+                SendKeys.SendWait("");
+            }//end catch
+            string setString = Console.ReadLine();
+            return setString;
+        }//Crashed
+        private static void DisplayMenuOptions()
+        {
+            Console.WriteLine("\t1. Add new set.");
+            Console.WriteLine("\t2. Remove a set.");
+            Console.WriteLine("\t3. Check for subset.");
+            Console.WriteLine("\t4. Check for element.");
+            Console.WriteLine("\t5. Perform set operation.");
+            Console.WriteLine("\t6. Perform set laws.");
+            Console.WriteLine("\t7. Clear all sets.");
+            Console.WriteLine("\t8. Reset set naming.");
+            Console.WriteLine("\t9. Learn more about sets.");
+            Console.WriteLine("\tX. Exit.");
+        }//DisplayMenuOptions
+    }//class
+}//namespace
---
+++ AdvancedSet/ClientMain.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using SetLibrary;
+using SetLibrary.Generic;
+using SetLibrary.Collections;
+namespace AdvancedSet
+{
+    public partial class Client
+    {
+        //This client will be for handling user inputs.
+        private static SetExtractionSettings<int> settings = new SetExtractionSettings<int>(",");
+
+        static void Main(string[] args)
+        {
+            //Setting up the back ground
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            //Create the set collection
+            ISetCollection<int> collection = new SetCollection<int>();
+            AddTestData(collection);
+
+            //Call the menu
+            Menu(collection);
+            //DisplaySets(collection);
+            //AddNewSet(collection);
+            //DisplaySets(collection);
+        }//Main
+        
+        public static void Menu(ISetCollection<int> sets)
+        {
+            //Option
+            string  option;
+            do
+            {
+                //Display menu here
+                Console.Clear();
+                Console.WriteLine("\tSet Application");
+                Console.WriteLine("\t===============");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("\tCurrent sets : ");
+                Console.WriteLine();
+                DisplaySets(sets);
+                //Display the current sets
+                DisplayMenuOptions();
+                Console.WriteLine();
+                Console.Write("\tOption : ");
+                option = Console.ReadLine().ToUpper();
+
+                bool success = int.TryParse(option, out int menuOption);
+                if (!success && option != "X")
+                    continue;
+                switch (menuOption)
+                {
+                    case 1: AddNewSet(sets); break; 
+                    case 2:RemoveSet(sets); break; 
+                    case 3: CheckForSubsets(sets); break;
+                    case 4: CheckForElement(sets); break; 
+                    case 5: PerformSetOperations(sets); break; 
+                    case 6: PerformSetLaws(sets); break;
+                    case 7: ClearAllSests(sets); break; 
+                    case 8:ResetNaming(sets); break; 
+                    case 9: LearnMoreAboutSets(); break;
+                    default:break;
+                }//end switch
+            } while (option != "X");
+        }//Menu
+
+        private static void LearnMoreAboutSets()
+        {
+            Console.Clear();
+            Console.WriteLine("\tLearning about sets.........");
+            AnyKey();
+        }//LearnMoreAboutSets
+
+        private static void ResetNaming(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tResetting naming.........");
+            AnyKey();
+        }//ResetNaming
+
+        private static void ClearAllSests(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tClearing All sets.........");
+            AnyKey();
+        }//ClearAllSests
+
+        private static void PerformSetLaws(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tPerformaing set laws.........");
+            AnyKey();
+        }//PerformSetLaws
+
+        private static void PerformSetOperations(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tPerformaing set operations.........");
+            AnyKey();
+        }//PerformSetOperations
+
+        private static void CheckForElement(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tChecking for elements.........");
+            AnyKey();
+        }//CheckForElement
+
+        private static void CheckForSubsets(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tChecking for subsets.........");
+            AnyKey();
+        }//CheckForSubsets
+
+        private static void RemoveSet(ISetCollection<int> sets)
+        {
+            Console.Clear();
+            Console.WriteLine("\tRemoving sets ......");
+            AnyKey();
+        }//RemoveSet
+
+        private static void AddNewSet(ISetCollection<int> sets)
+        {
+            //Variable for adding and looping
+            bool shouldExit = false;
+            bool should_add = false;
+
+            //Variables for displaying
+            string setString = "";
+            string error = "";
+            //Set variable
+            ICSet<int> newset = default;
+            do
+            {
+                //Display the new set header
+                DisplayNewSetHeader(sets);
+                //If the user entered an incorrect error
+                if (error == "")
+                    Console.WriteLine();
+                else
+                    DisplayError(error);
+
+                //Read the set string by passing the old string for modification
+                setString = ReadSetString("\tSet string : ", setString);
+                if(setString == "")
+                {
+                    shouldExit = true;
+                    should_add = false;
+                }
+                else
+                {
+                    try
+                    {
+                        //Here try to create the set, if braces are not matching or there's some sort of conversion error, catch it.
+                        newset = new GenericSet<int>(setString,settings);
+                        //Set the flag to exit
+                        shouldExit = true;
+                        should_add = true;
+                    }
+                    catch
+                    {
+                        //set the error flag
+                        error = "Braces are not matching or you have used none-integer values!!\n\tIf your previous attempt does not appear, press the \"up\" key on the keyboard ";
+                    }//end catch
+                }
+            } while (!shouldExit);
+
+            if (should_add)
+            {
+                //sets.Add(newset);
+                Console.Clear();
+                Console.WriteLine("\tThe Follwowing set will be added : ");
+                Console.WriteLine("\n\tOriginal expression : {0}", setString);
+                Console.WriteLine("\tEvaluated expression : {0}", newset);
+
+                Console.WriteLine();
+                Console.Write("\tDo you wish to add(Y = Yes/N = No) : ");
+                char option = Console.ReadLine().ToUpper()[0];
+                if (option == 'Y')
+                {
+                    sets.Add(newset);
+                    Console.WriteLine("\n\tSet was added successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("\n\tSet was not added.");
+                }
+            }
+            AnyKey();
+        }//AddNewSet
+        
+    }//class
+}//namespace
