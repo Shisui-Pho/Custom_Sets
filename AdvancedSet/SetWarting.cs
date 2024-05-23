a0f524e8e7d5446df334dc38637e795c3df82ff9
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2024-05-23 12:34:43 +0200

    Added a warning enumeration.

---
+++ AdvancedSet/SetWarting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedSet
+{
+    public enum SetWarting
+    {
+        Clear,
+        Remove_Set,
+        Remove_Element,
+        Reset_Naming
+    }//class
+}//namespace
